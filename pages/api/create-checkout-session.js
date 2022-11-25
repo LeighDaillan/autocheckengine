a4a511f1ab86e27893eae79f537d7d779afa0d1e
DaillanLeigh <francodaillanleigh@gmail.com>
2022-11-25 12:00:30 +0800

    Endpoint for checkout session in stripe

---
+++ pages/api/create-checkout-session.js
@@ -0,0 +1,48 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async (req, res) => {
+  const { items, email } = req.body;
+
+  const shortDescription = function (description) {
+    const maxLength = 150;
+
+    let trimmedString = description.substr(0, maxLength);
+
+    return (trimmedString = trimmedString.substr(
+      0,
+      Math.min(trimmedString.length, trimmedString.lastIndexOf(" "))
+    ));
+  };
+
+  const transformedItems = items.map((item) => ({
+    quantity: 1,
+    price_data: {
+      currency: "php",
+      unit_amount: item.price * 100,
+      product_data: {
+        name: item.name,
+        description: shortDescription(item.description),
+        images: [item.displayImage],
+      },
+    },
+  }));
+
+  console.log(transformedItems);
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    shipping_address_collection: {
+      allowed_countries: ["GB", "US", "CA"],
+    },
+    line_items: transformedItems,
+    mode: "payment",
+    success_url: "http://localhost:3000/success",
+    cancel_url: "http://localhost:3000/Basket",
+    metadata: {
+      email,
+      images: JSON.stringify(items.map((item) => item.image)),
+    },
+  });
+
+  res.status(200).json({ id: session.id });
+};
