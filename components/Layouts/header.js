cba0fb78cc0db48eaed50cae51d552a93a9c561d
DaillanLeigh <francodaillanleigh@gmail.com>
2022-11-25 14:04:16 +0800

    Update the ui of navigation bat

--- components/Layouts/header.js
+++ components/Layouts/header.js
@@ -10,7 +10,7 @@ import { useRouter } from "next/router";
 const Header = function () {
   const router = useRouter();
   return (
-    <nav className="grid grid-cols-2 lg:grid-cols-6 bg-white py-5 px-20 border-b">
+    <nav className="grid grid-cols-2 lg:grid-cols-4 bg-white py-5 px-20 border-b">
       {/* Logo Design */}
 
       <div className="pl-10">
@@ -26,7 +26,7 @@ const Header = function () {
       </div>
 
       {/* Navigation Link */}
-      <ul className="flex justify-between self-center px-10 text-lg col-span-3">
+      <ul className="flex justify-between self-center px-10 text-lg col-span-2  ">
         <li>
           <Tires />
         </li>
@@ -41,23 +41,17 @@ const Header = function () {
         </li>
       </ul>
       {/* Basket, Orders, and Account */}
-      <div className="flex justify-self-end self-center text-black mr-4 lg:col-span-2">
+      <div className="flex justify-self-end self-center text-black mr-4 ">
         <Link href="/Basket">
           <div className="cursor-pointer mx-5 flex self-center transform transition duration-500 p-2 hover:bg-gray-200 rounded-lg">
             <BsBagDash size="25" className="mr-1" />
-            <p className="text-lg">Basket</p>
-          </div>
-        </Link>
-        <Link href="/">
-          <div className="cursor-pointer mx-5 flex self-center transform transition duration-500 p-2 hover:bg-gray-200 rounded-lg">
-            <BsBoxSeam size="25" className="mr-1" />
-            <p className="text-lg">Orders</p>
+            <p className="text-xl">Basket</p>
           </div>
         </Link>
 
         <div className="cursor-pointer mx-5 flex self-center transform transition duration-500 p-2 hover:bg-gray-200 rounded-lg">
           <BsPersonCircle size="25" className="mr-1" />
-          <p className="text-lg">
+          <p className="text-xl">
             <Link href="/Account">Account</Link>
           </p>
         </div>
