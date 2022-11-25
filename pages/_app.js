a8447e9872ce3bef9f4abcab562f8197845f6c77
DaillanLeigh <francodaillanleigh@gmail.com>
2022-11-25 11:57:20 +0800

    Added session for next-auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import Layout from "../components/Layouts/layout";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   );
 }
-
-export default MyApp;
