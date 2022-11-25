b93a0b0af47d8d5316139a230b05a9741a3ceb83
DaillanLeigh <francodaillanleigh@gmail.com>
2022-11-25 11:48:43 +0800

    Add stripe checkout

--- components/Basket-Checkout.js
+++ components/Basket-Checkout.js
@@ -1,6 +1,28 @@
-import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { loadStripe } from "@stripe/stripe-js";
+const stripePromise = loadStripe(process.env.stripe_public_key);
+import axios from "axios";
 
 const BasketCheckout = function ({ basket }) {
+  const { data: session, status } = useSession();
+
+  const createCheckoutSession = async function () {
+    const stripe = await stripePromise;
+
+    const checkoutSession = await axios.post("/api/create-checkout-session", {
+      items: basket,
+      email: session.user.email,
+    });
+
+    // Redirect user/customer to stripe checkout
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+
+    // alert if have error
+    if (result.error) alert(result.error.message);
+  };
+
   const subTotal =
     basket.length === 0
       ? "0"
@@ -43,8 +65,15 @@ const BasketCheckout = function ({ basket }) {
         {(+subTotal.replaceAll(",", "") + shippingFee).toLocaleString("en-US")}
       </h2>
 
-      <button className="bg-black text-white px-3 py-2 rounded-full cursor-pointer text-xl my-5 w-full">
-        Checkout
+      <button
+        role="link"
+        disabled={!session}
+        onClick={createCheckoutSession}
+        className={`bg-black text-white px-3 py-2 rounded-full  text-xl my-5 w-full ${
+          !session ? "opacity-75" : "cursor-pointer"
+        }`}
+      >
+        {!session ? "Sign in to Proceed" : "Checkout"}
       </button>
     </>
   );
