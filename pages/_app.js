ba72a3edfc27155232a28abcbad1d9f3d9410152
DaillanLeigh <francodaillanleigh@gmail.com>
2022-11-29 11:43:38 +0800

    Add context api

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,18 @@
 import "../styles/globals.css";
 import Layout from "../components/Layouts/layout";
 import { SessionProvider } from "next-auth/react";
-
+import CheckoutContextProvider from "../components/CheckoutContextProvider";
 export default function MyApp({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
     <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <CheckoutContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </CheckoutContextProvider>
     </SessionProvider>
   );
 }
