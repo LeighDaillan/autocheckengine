72b98a30b7617c2ba28f441bde6452ab2e5aec5b
DaillanLeigh <francodaillanleigh@gmail.com>
2022-12-01 17:43:58 +0800

    Used the user data from context api

--- pages/Account/index.js
+++ pages/Account/index.js
@@ -1,22 +1,21 @@
+import { useContext } from "react";
 import { BsFillPersonFill } from "react-icons/bs";
 import Image from "next/image";
 import { useSession, signOut, getSession } from "next-auth/react";
+import { CheckoutContext } from "../../components/CheckoutContextProvider";
 
 const Account = function () {
-  const { data: status } = useSession();
-  const email = status?.user.email;
-  const name = status?.user.email;
-
+  const { user } = useContext(CheckoutContext);
   return (
     <main className="bg-gray-200 h-screen py-20">
       <section className="bg-white rounded-lg max-w-xl mx-auto drop-shadow-2xl py-4 px-10">
         <p className="text-xl">You are signed in as</p>
         <div className="flex p-3 my-5">
-          {!status.user.image ? (
+          {!user.user.image ? (
             <BsFillPersonFill size={30} />
           ) : (
             <Image
-              src={status?.user.image}
+              src={user?.user.image}
               width="50"
               height="50"
               alt="User Profile"
@@ -24,8 +23,8 @@ const Account = function () {
             />
           )}
           <div className="self-center ml-2">
-            <h5 className="text-lg font-bold">{status?.user.name}</h5>
-            <span>{status?.user.email}</span>
+            <h5 className="text-lg font-bold">{user?.user.name}</h5>
+            <span>{user?.user.email}</span>
           </div>
         </div>
         <button
